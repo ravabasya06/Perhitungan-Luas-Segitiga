ba88f429b4c6de617f77aab93504e1cbc537375e
rav <115064706+ravabasya06@users.noreply.github.com>
2022-10-19 13:23:38 +0700

    Create luas segitiga.py

---
+++ luas segitiga.py
@@ -0,0 +1,13 @@
+def ulang():
+    
+    alas = float(input("Masukkan alas segitiga : "))
+    tinggi = float(input("Masukkan tinggi segitiga : "))
+    print("Luas : "+str(float(1/2*alas*tinggi)))
+    pengulangan = input("Lakukan perhitungan lagi? ya / tidak : ")
+    if pengulangan == "ya":
+        ulang()
+    elif pengulangan == "tidak":
+        print("Ok")
+    else:
+        print("Saya tidak mengerti perintah anda, saya akan menganggap anda menjawab tidak.")
+ulang()
